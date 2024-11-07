c589fc151db859dd741e914272497b87e2a652af
Ivan K <krylov.r00t@gmail.com>
2024-11-07 13:22:57 +0300

    Count symbols exported by libR.so

--- posts/2024-12-12-non-api-use/precomputed.R
+++ posts/2024-12-12-non-api-use/precomputed.R
@@ -1,5 +1,23 @@
-cpdb <- tools::CRAN_package_db()
-checks <- subset(tools::CRAN_check_details(), Package == 'data.table')
+library(depcache) # TODO: uncache everything before submission
+library(data.table)
+
+symbols %<-% fread(
+	# most likely implies R on GNU/Linux built with --enable-R-shlib
+	paste('nm -gDP', file.path(R.home('lib'), 'libR.so')),
+	fill = TRUE, col.names = c('name', 'type', 'value', 'size')
+)[
+	type %in% c('B', 'D', 'R', 'T') # don't care about [weak] imports
+][,
+	type := fcase(
+		type == 'B', 'variable',
+		type == 'D', 'data',
+		type == 'R', 'read-only data',
+		type == 'T', 'function'
+	)
+][]
+
+cpdb %<-% tools::CRAN_package_db()
+checks %<-% subset(tools::CRAN_check_details(), Package == 'data.table')
 
 when <- Sys.Date()
-save(cpdb, checks, when, file = 'precomputed.rda')
+save(cpdb, checks, symbols, when, file = 'precomputed.rda', compress = 'xz')
