179d69bd48c63815f25ec1f828b812d110ce51c8
Ivan K <krylov.r00t@gmail.com>
2024-11-29 17:45:32 +0300

    Update precomputed.* and fix its use

--- posts/2024-12-12-non-api-use/precomputed.R
+++ posts/2024-12-12-non-api-use/precomputed.R
@@ -1,10 +1,9 @@
-library(depcache) # TODO: uncache everything before submission
 library(data.table)
 
 # The results are not reproducible because they depend on both the R-devel
 # version and the data.table-git version, hence the pre-computation.
 
-symbols %<-% fread(
+symbols <- fread(
 	# most likely implies R on GNU/Linux built with --enable-R-shlib
 	paste('nm -gDP', file.path(R.home('lib'), 'libR.so')),
 	fill = TRUE, col.names = c('name', 'type', 'value', 'size')
@@ -19,7 +18,7 @@ symbols %<-% fread(
 	)
 ][]
 
-DTsymbols %<-% fread(
+DTsymbols <- fread(
 	# again, only tested on GNU/Linux
 	paste('nm -gDP', system.file(
 		file.path('libs', 'data_table.so'), package = 'data.table'
@@ -44,7 +43,7 @@ setdiff(
 ) |> setdiff(
 	# and also skip variables because they are omitted in funAPI
 	symbols[type == 'variable', name]
-) %->% DTnonAPI
+) -> DTnonAPI
 # which ones does R CMD check _not_ complain about... yet?
 DTnonAPI_yet <- setdiff(DTnonAPI, tools:::nonAPI)
 
@@ -55,9 +54,7 @@ getNonAPI <- function(ver,
 		ver
 	)
 ) {
-	ee <- parse(text = cache(
-		readLines(url)
-	))
+	ee <- parse(text = readLines(url))
 	for (e in ee) {
 		if (
 			is.call(e) && length(e) == 3 &&
@@ -72,9 +69,9 @@ nonAPI.4_4 <- getNonAPI('4-4')
 nonAPI.trunk <- getNonAPI(url = 'https://svn.r-project.org/R/trunk/src/library/tools/R/sotools.R')
 
 # CRAN package metadata and check results
-cpdb %<-% tools::CRAN_package_db()
+cpdb <- tools::CRAN_package_db()
 needscomp <- cpdb[,'NeedsCompilation'] == 'yes'
-checks %<-% tools::CRAN_check_details()
+checks <- tools::CRAN_check_details()
 dtchecks <- subset(checks, Package == 'data.table')
 
 when <- Sys.Date()
